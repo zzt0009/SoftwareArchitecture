b07230f66913f7f31195f842a701226e003c6289
Ziyan Tian <zzt0009@tigermail.auburn.edu>
2019-08-28 10:09:58 -0500

    Finished class finder

---
+++ Assignment2/src/main/java/Finder.java
@@ -0,0 +1,28 @@
+public class Finder {
+    public Integer findMax(int[] intArray){
+        if(intArray.length == 0){
+            return null;
+        }
+        int max = Integer.MIN_VALUE;
+        for(int num : intArray){
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public Integer findMin(int[] intArray){
+        if(intArray.length == 0){
+            return null;
+        }
+        int min = Integer.MAX_VALUE;
+        for(int num : intArray){
+            if(num < min){
+                min = num;
+            }
+        }
+        return min;
+    }
+
+}
